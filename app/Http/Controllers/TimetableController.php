2a72207ed347ef355a6698586bd9c1a53137802a
robinyang <u1352883@unimail.hud.ac.uk>
2017-02-10 16:49:13 +0000

    Add to timetable database and view
    Create new timetable database class, model and controller
    
    Add new lesson to timetable
    
    view added lesson
    
    Array merge on user + request fields (input fields)

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Timetable;
+use Auth;
 
 class TimetableController extends Controller
 {
 
+    public function showTimetable(){
+
+        $timetable = Timetable::all();
+        return view('page.dashboard', compact('timetable'));
+    }
+
+    public function addForm(){
+
+        return view('forms.addLesson');
+    }
+
+    public function addTimetable(Request $request, Timetable $timetable){
+
+        $this->validate($request,['module'=>'required', 'lecturer_name'=>'required', 'location'=>'required',
+                                  'time'=>'required', 'finish'=>'required', 'date'=>'required']);
+
+        $new = new Timetable($request->all());
+        $new->user_id = Auth::user()->id;
+        $new->save();
+        return back();
+    }
 }
--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -3,15 +3,27 @@
 
 @section('content')
 
+
+    <p>Lessons</p>
+    @foreach($timetable as $lesson)
+        Module: {{$lesson->module}}<br>
+        Tutor: {{$lesson->lecturer_name}}<br>
+        Location: {{$lesson->location}}<br>
+        Start time: {{$lesson->time}}<br>
+        End time: {{$lesson->finish}}<br>
+        Date {{$lesson->date}}<br>
+    @endforeach
+
+    <hr>
+
+
 <a href="{{ url('#') }}">Add Event</a> <br>
 
 <a href="{{ url('#') }}">Upcoming Event</a> <br>
 
 <a href="{{ url('#') }}">Change Event</a> <br>
 
-
-
-<a href="{{ url('#') }}">Add Timetable</a> <br>
+<a href="{{ url('/addTimetable') }}">Add Timetable</a> <br>
 
 
 
